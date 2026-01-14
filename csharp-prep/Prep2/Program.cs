4f218db339d3b9902df09236004d4e1d0ba39ed6
huntanderson822 <and25027@byui.edu>
2026-01-13 19:35:59 -0700

    Finsihed the core requirments. When I have time, I am going to finish the streth challenge so I can stay in a developer mindset. I can't wait to learn functions, it'll make this easier.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // ASk the user for their grade percentage & convert it to a integer
+        Console.WriteLine("What is your grade percentage? (Just the digits) ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry mate, you did not pass. Better luck next time!");
+        }
     }
 }

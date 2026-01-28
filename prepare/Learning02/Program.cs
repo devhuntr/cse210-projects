0fa4896235d098d35be8b081eea8f246046a62b7
huntanderson822 <and25027@byui.edu>
2026-01-27 23:47:48 -0700

    I finished the class lesson. It is new, but I will figure it out!

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobtitle = "Software Engineer";
+        job1._startyear = 2015;
+        job1._endyear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "AWS";
+        job2._jobtitle = "Solutions Architect";
+        job2._startyear = 2020;
+        job2._endyear = 2024;
+
+        Resume myresume = new Resume();
+        myresume._name = "Hunter Anderson";
+
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.Display();
+
     }
 }
